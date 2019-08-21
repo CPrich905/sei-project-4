84280e0b12c14431aae2d301584cda933c528c77
CPrich905 <charlie.prichard@gmail.com>
2019-08-21 15:31:43 +0100

    heroku test

--- config/router.py
+++ config/router.py
@@ -10,7 +10,10 @@ app.register_blueprint(tags.api, url_prefix='/api')
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
+
 def catch_all(path):
     if os.path.isfile('dist/' + path):
         return app.send_static_file(path)
     return app.send_static_file('index.html')
+
+
